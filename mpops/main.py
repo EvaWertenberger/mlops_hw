a91df684f85ae907e81f7e305952e65bd6e9a193
eva <evajjikk@gmail.com>
2024-10-15 00:38:51 +0500

    add new code

--- mpops/main.py
+++ mpops/main.py
@@ -1,6 +1,13 @@
-def summ_numbers(a: int, b:int) -> int:
-    return a + b
+import utils
 
-a = 5
-b = 10
-print(summ_numbers(a,b))
+
+# Test the division function
+print(utils.div_numbers(10, 2))  # Output: 5
+
+try:
+    print(utils.div_numbers(10, 0))
+except ValueError as e:
+    print(e)  # Output: b cannot be zero
+
+# Test the summation function
+print(utils.summ_numbers(10, 2))  # Output: 12
